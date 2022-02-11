2696468cd2ca765f3273979e08c14dce91bc1ec8
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-02-11 13:32:21 +0000

    added homepage

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Somtin Somtin',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
