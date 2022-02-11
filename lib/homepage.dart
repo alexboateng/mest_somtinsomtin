081113185e2ea220f748a3b6551641fb1db5cf0a
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-02-11 19:14:06 +0000

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -13,9 +15,68 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Somtin Somtin"),
+        backgroundColor: Colors.pink,
       ),
-      body: Container(
-        color: Colors.red,
+      body: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int position) {
+            Widget content = Container();
+            if (position == 0) {
+              content = _balanceView();
+            } else {
+              content = _discountView();
+            }
+            return content;
+          }),
+    );
+  }
+
+  Widget _discountView() {
+    return Container(
+      child: Text("_discountView"),
+    );
+  }
+
+  Widget _balanceView() {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              margin: EdgeInsets.only(bottom: 15),
+              // constraints: BoxConstraints.expand(),
+              // color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Total Gift Voucher"),
+                      Text(
+                        "Price 10",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  // Expanded(child: Text("1")),
+                  Text(
+                    "Buy gift voucher",
+                    style: TextStyle(
+                        color: Colors.pink, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Icon(
+            Icons.picture_in_picture_alt,
+            size: 150,
+          )
+        ],
       ),
     );
   }
